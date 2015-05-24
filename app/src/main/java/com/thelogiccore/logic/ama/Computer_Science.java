7222ce096e45ec7e6fe9376d6d59c1a1119aab7c
Shawn Hice <shawn@thelogiccore.com>
2015-05-23 22:38:30 -0700

    bv,jlk.jkl.,

--- app/src/main/java/com/thelogiccore/logic/ama/Computer_Science.java
+++ app/src/main/java/com/thelogiccore/logic/ama/Computer_Science.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Shawn on 5/23/2015.
@@ -13,6 +15,19 @@ public class Computer_Science extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.compsci_fragment, container, false);
+        final View view = inflater.inflate(R.layout.compsci_fragment,
+                container, false);
+        final QuestionBank qBank = new QuestionBank();
+        Button button = (Button) view.findViewById(R.id.compsci_gen);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                TextView question = (TextView) view.findViewById(R.id.question_compsci);
+                question.setText(qBank.getRandomQuestion("Computer Science").getQuestion());
+            }
+        });
+        return view;
     }
 }
