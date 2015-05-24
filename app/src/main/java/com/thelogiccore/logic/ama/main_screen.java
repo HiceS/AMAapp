eb408f28594b85d66468003d3364de5d0be57979
Shawn Hice <shawn@thelogiccore.com>
2015-05-23 20:17:46 -0700

    Some changes since last time...

--- app/src/main/java/com/thelogiccore/logic/ama/main_screen.java
+++ app/src/main/java/com/thelogiccore/logic/ama/main_screen.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by Shawn on 5/23/2015.
@@ -15,5 +16,6 @@ public class main_screen extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main_screen, container, false);
+       // ImageView gif = (ImageView)findViewById(R.id.formsf14);
     }
 }
