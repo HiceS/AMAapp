d9520ca4ffd8d176b65ef472411957ebcc12b67b
yozaner1324 <kcirtap1324@gmail.com>
2015-05-23 20:13:28 -0700

    more questions

--- question/src/question/QuestionBank.java
+++ question/src/question/QuestionBank.java
@@ -21,23 +21,25 @@ public class QuestionBank {
     {
         questions = new ArrayList<>();
         
-        QuestionBank.add("How much does Fauzi weigh?", "Generic");
+        QuestionBank.add("How much does Fauzi weigh?", "Awkward");
+        QuestionBank.add("Do you love Fauzi?", "Awkward");
+        QuestionBank.add("Do you love me?", "Awkward");
         QuestionBank.add("How do you feel about linux?", "Computer Science");
         QuestionBank.add("What is your favorite programing language?", "Computer Science");
-        QuestionBank.add("What should we name our first child?", "Generic");
+        QuestionBank.add("What should we name our first child?", "Awkward");
         QuestionBank.add("Do you even lift?", "Generic");
         QuestionBank.add("What do you do for fun?", "Generic");
         QuestionBank.add("How much can you bench?", "Generic");
         QuestionBank.add("What is your favorite feature?", "Generic");
-        QuestionBank.add("Why are ou so attracted to me?", "Generic");
+        QuestionBank.add("Why are ou so attracted to me?", "Awkward");
         QuestionBank.add("What are you doing right now?", "Generic");
         QuestionBank.add("Do you like to cook?", "Generic");
         QuestionBank.add("Do you have any pets?", "Generic");
         QuestionBank.add("Do you have any siblings?", "Generic");
         QuestionBank.add("How many types of cheese are there in the world?", "Generic");
-        QuestionBank.add("How fast can you grow a beard?", "Generic");
+        QuestionBank.add("How fast can you grow a beard?", "Awkward");
         QuestionBank.add("Harry Potter or Twilight?", "Generic");
-        QuestionBank.add("Do you know the muffin man?", "Generic");
+        QuestionBank.add("Do you know the muffin man?", "Awkward");
         QuestionBank.add("What do you think happens after you die?", "Generic");
         QuestionBank.add("What is your favorite food?", "Generic");
         QuestionBank.add("What is the meaning of life?", "Generic");
