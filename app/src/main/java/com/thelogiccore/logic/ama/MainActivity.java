24ed529a2c862ee1db6c612a4185c180ad3227b5
t_grahj <jamie.graham@autodesk.com>
2015-05-23 22:00:03 -0700

    Options Menu creation

--- app/src/main/java/com/thelogiccore/logic/ama/MainActivity.java
+++ app/src/main/java/com/thelogiccore/logic/ama/MainActivity.java
@@ -113,6 +113,7 @@ public class MainActivity extends ActionBarActivity
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
             getMenuInflater().inflate(R.menu.main, menu);
+            getMenuInflater().inflate(R.menu.options_menu, menu);
             restoreActionBar();
             return true;
         }
